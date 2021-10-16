82aa541a068f50be62315cf39759b9cf834f3ea3
Daisuke-Koguchi <ds05080906@gmail.com>
2021-10-16 15:52:24 +0000

    ruby-examの提出

---
+++ ruby_exam/janken.rb
@@ -0,0 +1,50 @@
+class Player
+  def hand
+    puts "数字を入力してください。"
+    puts "0:グー, 1:チョキ, 2:パー"
+    input_hand =gets.chomp
+    while true
+       if input_hand.include?("0" "1" "2")
+        return input_hand
+       else
+        puts "0〜2の数字を入力してください。"
+        puts "0:グー, 1:チョキ, 2:パー"
+        break
+       end
+    end
+    input_hand.to_i
+  end
+end
+class Enemy
+  def hand
+    rand(0..2)
+  end
+end
+class Janken
+  def pon(player_hand, enemy_hand)
+    janken = ["グー", "チョキ", "パー"]
+    puts "相手の手は#{janken[enemy_hand]}です。"
+    if (player_hand - enemy_hand + 3) % 3 == 0
+      puts "あいこ"
+      true
+    elsif (player_hand - enemy_hand + 3) % 3 == 2
+      puts "あなたの勝ちです"
+      false
+    else
+      puts "あなたの負けです"
+      false
+    end
+  end
+end
+class GameStart
+  def self.jankenpon
+    player = Player.new
+    enemy = Enemy.new
+    janken = Janken.new
+    next_game = true
+    while  next_game 
+      next_game = janken.pon(player.hand, enemy.hand)
+    end
+  end
+end
+GameStart.jankenpon
